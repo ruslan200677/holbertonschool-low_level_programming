f6564fc535287da625ed7de4dd1519d64915857f
ruslan200677 <qaqli20033@gmail.com>
2024-06-23 12:28:03 +0000

    1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * @x: entry parametr
+ * main - check the code
+ * Return: Always 0
+ */
+
+int _abs(int x)
+{
+
+	if (x < 0)
+	{
+	return (-x);
+	}
+	else
+	{
+	return (x);
+	}
+}
