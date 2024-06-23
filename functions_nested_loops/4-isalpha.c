ce91b026d6baf47b0bc1b8c42533daf62079d859
ruslan200677 <qaqli20033@gmail.com>
2024-06-23 12:20:03 +0000

    1

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,23 +1,24 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _islower - entry point
+ * _isalpha - entry point
  * Description: This function prints the alphabet, in lowercase
  * @c: entry parametr
  * main - check the code
  * Return: Always 0
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 
-	if (c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 	return (1);
 	}
 	else
 	{
 	return (0);
+	printf("-");
 	}
 
 	return (0);
