bdd8cd949646e5643f305ce09ac0e03accd2e2be
ruslan200677 <qaqli20033@gmail.com>
2024-06-23 12:14:05 +0000

    1

---
+++ functions_nested_loops/3-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * @c: entry parametr
+ * main - check the code
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+	return (0);
+}
