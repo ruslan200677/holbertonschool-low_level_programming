e6c75b281fa44060c39b825022dd0a08339b6b14
ruslan200677 <qaqli20033@gmail.com>
2024-06-26 11:47:16 +0000

    1

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * salam
+*/
+void print_square(int size)
+{
+int inc1, inc2;
+if (size > 0)
+{
+for (inc1 = 0; inc1 < size; inc1++)
+{
+for (inc2 = 0; inc 2 < (size - 1); inc2++)
+_putchar ('#')
+_putchar ('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
