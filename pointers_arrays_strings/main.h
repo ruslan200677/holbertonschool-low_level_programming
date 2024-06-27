fb72b70733e07a200b5b6de8d2856b8c89dd5bb1
ruslan200677 <qaqli20033@gmail.com>
2024-06-27 12:41:57 +0000

    3-puts.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +12,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c)
+#endif
