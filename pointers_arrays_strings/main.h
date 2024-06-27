4717ed32e8cd0a06b2eb309a457eaa012ba240e6
ruslan200677 <qaqli20033@gmail.com>
2024-06-27 12:24:48 +0000

    1

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,3 +12,16 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
