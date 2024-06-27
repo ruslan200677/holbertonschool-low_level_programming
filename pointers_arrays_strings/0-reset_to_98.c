1bfe986123e1e96fe6fa6d10cf3559684309981e
ruslan200677 <qaqli20033@gmail.com>
2024-06-27 10:09:10 +0000

    1

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * salam
- * Return : nothing
- */
+*reset_to_98 - reset value of an integer to 98
+*@n: parameter targeted
+*Return : nothing
+*/
 
 void reset_to_98(int *n)
-{	
-*n = 98
+{
+*n = 98;
 }
