413086f89346bb0592c30a0324b164dcad825513
ruslan200677 <qaqli20033@gmail.com>
2024-06-29 08:59:30 +0000

    1

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcpy - copy a string to a buffer destination
+ * @src: pointer to a string
+ * @dest: buffer pointer destination
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+#include "main.h"
