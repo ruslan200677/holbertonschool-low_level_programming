a2e49edfe028e0e9bd18046c2a6f20761df825dc
ruslan200677 <qaqli20033@gmail.com>
2024-07-02 09:22:55 +0000

    1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * irfjrij
+*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
