69cec6c6b76e693801ab43493fad79778d79eee4
ruslan200677 <qaqli20033@gmail.com>
2024-08-01 12:38:33 +0400

    1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - main
+ * @h; clone of our linkedlist
+ * Return; length
+ */
+
+size_t list_len(const list_t *h);
+
+{
+	int length = 0;
+	while (h != NULL)
+	{
+		length++;
+		h = h-> next;
+	}
+	return (length);
+}
+
