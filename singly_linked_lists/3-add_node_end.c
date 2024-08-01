fa70af03387cd98b70fecbd467240dcc47f34bfd
ruslan200677 <qaqli20033@gmail.com>
2024-08-01 16:22:42 +0400

    1

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h>
+
+/**
+ * add_node_end - man
+ * @head: for linked list
+ * @str: var1
+ * Return: linked list
+ */
+
+list_t *add_node_end(list_t **head, const char *str);
+{
+	int length_nod
